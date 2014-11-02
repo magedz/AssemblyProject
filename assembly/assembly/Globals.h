856772497a5edfe009d2efcd287228c652aefbbc
magedz <magedmekawy@aucegypt.edu>
2014-11-02 19:00:04 +0200

    newwwww

--- assembly/assembly/Globals.h
+++ assembly/assembly/Globals.h
@@ -15,9 +15,9 @@ public:
 	vector <string> funcList;
 	vector<int> labels; // label0 is in index 0 ..etc
 	vector<reg> registers;
-	vector<int> registerNames;
+	//vector<int> registerNames;
 	vector<char> M;
-	vector<char>R;
+	//vector<char>R;
 	vector<int> binaryList;
 	//Instruction* PC;
 
